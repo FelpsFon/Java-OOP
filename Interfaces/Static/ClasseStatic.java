04c6056f69150abcfc9b0c6a3ebd8b71c6342fd2
Felipe Fontoura <felipelfontoura@gmail.com>
2022-10-21 08:57:53 -0300

    Interfaces

---
+++ Interfaces/Static/ClasseStatic.java
@@ -0,0 +1,14 @@
+package Static
+
+public class CasseStatic {
+  static int i = 46;
+  int j = 26;
+
+  static void imprime(String s) {
+    System.out.println(s);
+  }
+
+  static void incrementaI() { i++; }
+
+  void incrementaJ() { j++; }
+}
---
+++ Interfaces/Static/TesteStatic.java
@@ -0,0 +1,9 @@
+package Static
+
+public class TesteStatic {
+  public static void main(String[] args) {
+    System.out.println("i: " + ClasseStatic.i);
+    ClasseStatic.incrementaI();
+    ClasseStatic.j;
+  }
+}
